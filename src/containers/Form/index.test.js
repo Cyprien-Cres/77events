4c68859317165c18f9996b3238a094d932e48d13
Cyprien <cres.cyprien@gmail.com>
2024-01-16 18:48:00 +0100

    Résolution des console.error

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -22,8 +22,6 @@ describe("When Events is created", () => {
         })
       );
       await screen.findByText("En cours");
-      // eslint-disable-next-line no-promise-executor-return
-      await new Promise((r) => setTimeout(r, 1000)); // fixed scenario 7
       await screen.findByText("Envoyer");
       expect(onSuccess).toHaveBeenCalled();
     });
--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -21,8 +21,6 @@ describe("When Form is created", () => {
         })
       );
       await screen.findByText("En cours");
-      // eslint-disable-next-line no-promise-executor-return
-      await new Promise((r) => setTimeout(r, 1000)); // fixed scenario 7
       await screen.findByText("Message envoyé !");
     });
   });
